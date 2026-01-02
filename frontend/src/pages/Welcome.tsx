1d7b57f7797a771b2511163e76fd7128e56383f7
Shazzz <nickshahbaz135@gmail.com>
2026-01-02 16:11:30 -0500

    added lightpillar background to welcome page

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,16 +1,34 @@
+/* Components */
 import GetStarted from "../components/welcome/GetStarted";
 import WhyUs from "../components/welcome/WhyUs";
 import Reviews from "../components/welcome/Reviews";
+/* Utils */
+import LightPillar from "../utils/LightPillar";
+
+
 
 function Welcome() {
     return (
-        <>
-            <h1 className="text-3xl font-bold">*This is the Welcome Page*</h1>
-            <br />
-            <GetStarted />
-            <WhyUs />
-            <Reviews />
-        </>
+        <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+            <LightPillar
+                topColor="#338CFF"
+                bottomColor="#4FC3FF"
+                intensity={1.0}
+                rotationSpeed={0.5}
+                glowAmount={0.005}
+                pillarWidth={3.0}
+                pillarHeight={0.4}
+                noiseIntensity={0.5}
+                pillarRotation={75}
+                interactive={false}
+                mixBlendMode="normal"
+            />
+            <div style={{ position: 'relative', zIndex: 1 }}>
+                <GetStarted />
+                <WhyUs />
+                <Reviews />
+            </div>
+        </div>
     );
 }
 
